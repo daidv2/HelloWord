e9cbdaae4caf5bffbaee020e587254de39e6cb1b
DaiDV <daidv@rikkeisoft.com>
2021-04-24 14:04:05 +0700

    Spring MVC 3 - Thực hành tạo project Spring MVC Hello World - XML Config

---
+++ src/main/java/com/trungtamjava/springmvc/HelloController.java
@@ -0,0 +1,21 @@
+package com.trungtamjava.springmvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String sayHello(ModelMap map) {
+        map.addAttribute("msg", "Trung tam java");
+        return "hello";
+    }
+}
